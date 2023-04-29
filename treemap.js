40a4fbe6914b803155a95e8a5fa319c6a4c2c6b7
Jefferson Lucena <53662380+jdlucena@users.noreply.github.com>
2023-04-29 12:16:35 -0300

    Create treemap.js

---
+++ treemap.js
@@ -0,0 +1,111 @@
+{
+	"table": "FAT",
+	"rows":
+	[
+		{
+			"Competência": "01/2023",
+			"Setor": "AMBULATORIO - MEDICOS                   ",
+			"Valor": 10201.9
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "CENTRO DE REAB. DE FIS. LABIO. DO RN    ",
+			"Valor": 3027.71
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "LABORATORIO                             ",
+			"Valor": 3833.34
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "NUCLEO DE TRATAM. DE OBESIDADE INFANT.  ",
+			"Valor": 1802.8
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "RAIO X                                  ",
+			"Valor": 2316.02
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "SALA DE GESSO                           ",
+			"Valor": 3392.48
+		},
+		{
+			"Competência": "01/2023",
+			"Setor": "HOSPITAL DIA                            ",
+			"Valor": 2268.01
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "AMBULATORIO - MEDICOS                   ",
+			"Valor": 9656.6
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "CENTRO DE REAB. DE FIS. LABIO. DO RN    ",
+			"Valor": 4014.83
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "LABORATORIO                             ",
+			"Valor": 3647.9
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "NUCLEO DE TRATAM. DE OBESIDADE INFANT.  ",
+			"Valor": 1902.1
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "RAIO X                                  ",
+			"Valor": 1930.28
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "SALA DE GESSO                           ",
+			"Valor": 3012.83
+		},
+		{
+			"Competência": "02/2023",
+			"Setor": "HOSPITAL DIA                            ",
+			"Valor": 1856.8
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "AMBULATORIO - MEDICOS                   ",
+			"Valor": 13686.5
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "RAIO X                                  ",
+			"Valor": 2905.29
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "SALA DE GESSO                           ",
+			"Valor": 5715.78
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "HOSPITAL DIA                            ",
+			"Valor": 2428
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "LABORATORIO                             ",
+			"Valor": 4554.94
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "NUCLEO DE TRATAM. DE OBESIDADE INFANT.  ",
+			"Valor": 1771.9
+		},
+		{
+			"Competência": "03/2023",
+			"Setor": "CENTRO DE REAB. DE FIS. LABIO. DO RN    ",
+			"Valor": 5285.92
+		}
+	]
+}
